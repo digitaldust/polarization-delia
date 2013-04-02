bee77155e523cfdbac639e8856ee813e67257248
digitaldust <digitaldust@digitaldust.lan>
2013-04-02 16:08:13 +0200

    package renamed

---
+++ dypol/src/org/nlogo/extensions/dypol/ClassManager.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.nlogo.extensions.dypol;
+
+/**
+ *
+ * @author digitaldust
+ */
+public class ClassManager {
+    
+}
